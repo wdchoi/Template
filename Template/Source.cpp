3ea0dd7611066372331445fb6e9bf5fe75bcb452
wdchoi <wdchoi.dgu@outlook.com>
2016-10-11 20:08:01 +0900

    fdsa
    fdsaf

--- Template/Source.cpp
+++ Template/Source.cpp
@@ -1,63 +1,12 @@
-#include <GLFW/glfw3.h>
+
 #include "iostream"
 #include <string>
 #include <vector>
-const int width = 640;
-const int height = 480;
+#include "My2D.h"
 
-//color = (Red, Green, Blue)
-float *pixels = new float[width * height * 3];
-//float *pixels = (float*)malloc(sizeof(float)*width*height * 3);
-void drawPixel(const int& i, const int& j, const float& red, const float& green, const float& blue)
-{
-	pixels[(i + width* j) * 3 + 0] = red;
-	pixels[(i + width* j) * 3 + 1] = green;
-	pixels[(i + width* j) * 3 + 2] = blue;
-}
 
-void drawLine(const int& i0, const int& j0, const int& i1, const int& j1, const float& red, const float& green, const float& blue)
-{
-	if (i1 > i0) {
-		if (j1 >= j0) {
-			for (int i = i0; i <= i1; i++)
-			{
-				const int j = (j1 - j0)*(i - i0) / (i1 - i0) + j0;
+MyPainter2D Obj_2D;
 
-				drawPixel(i, j, red, green, blue);
-			}
-		}
-		else {
-			for (int i = i0; i <= i1; i++)
-			{
-				const int j = j0 - (j0 - j1)*(i - i0) / (i1 - i0);
-
-				drawPixel(i, j, red, green, blue);
-			}
-		}
-	}
-	else if (i1 < i0) {
-		if (j1 >= j0) {
-			for (int i = i1; i <= i0; i++)
-			{
-				const int j = j1 - (j1 - j0)*(i - i1) / (i0 - i1);
-				drawPixel(i, j, red, green, blue);
-			}
-		}
-		else {
-			for (int i = i1; i <= i0; i++)
-			{
-				const int j = j1 + (j0 - j1)*(i - i1) / (i0 - i1);
-
-				drawPixel(i, j, red, green, blue);
-			}
-		}
-	}
-	else {
-		for (int j = j0; j <= j1; j++) {
-			drawPixel(i0, j, 1.0f, 0.0f, 0.0f);
-		}
-	}
-}
 class Box {
 private:
 	int center_x=75, center_y=375;
@@ -65,10 +14,10 @@ private:
 public:
 	
 	void draw() {
-		drawLine(center_x - x_edge / 2, center_y - y_edge / 2, center_x + x_edge / 2, center_y - y_edge / 2, 1.0f, 0.0f, 0.0f);
-		drawLine(center_x + x_edge / 2, center_y - y_edge / 2, center_x + x_edge / 2, center_y + y_edge / 2, 1.0f, 0.0f, 0.0f);
-		drawLine(center_x - x_edge / 2, center_y + y_edge / 2, center_x + x_edge / 2, center_y + y_edge / 2, 1.0f, 0.0f, 0.0f);
-		drawLine(center_x - x_edge / 2, center_y - y_edge / 2, center_x - x_edge / 2, center_y + y_edge / 2, 1.0f, 0.0f, 0.0f);
+		Obj_2D.drawLine(center_x - x_edge / 2, center_y - y_edge / 2, center_x + x_edge / 2, center_y - y_edge / 2, 1.0f, 0.0f, 0.0f);
+		Obj_2D.drawLine(center_x + x_edge / 2, center_y - y_edge / 2, center_x + x_edge / 2, center_y + y_edge / 2, 1.0f, 0.0f, 0.0f);
+		Obj_2D.drawLine(center_x - x_edge / 2, center_y + y_edge / 2, center_x + x_edge / 2, center_y + y_edge / 2, 1.0f, 0.0f, 0.0f);
+		Obj_2D.drawLine(center_x - x_edge / 2, center_y - y_edge / 2, center_x - x_edge / 2, center_y + y_edge / 2, 1.0f, 0.0f, 0.0f);
 	}
 };
 
@@ -88,7 +37,7 @@ public:
 				int f = (r_radius - b)*(r_radius - b) + (r_radius - a)*(r_radius - a) - r_radius*r_radius;
 				if (f < 0 && -200 < f)
 				{
-					drawPixel(center_x - r_radius + b, center_y - r_radius + a, red, green, blue);
+					Obj_2D.drawPixel(center_x - r_radius + b, center_y - r_radius + a, red, green, blue);
 				}
 			}
 		}
@@ -111,39 +60,11 @@ public:
 
 
 int main() {
-	GLFWwindow* window;
+	Obj_2D.initialize();
 
-	/* Initialize the library */
-	if (!glfwInit())
-		return -1;
-
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window)
+	while (Obj_2D.Close())
 	{
-		glfwTerminate();
-		return -1;
-	}
-
-	/* Make the window's context current */
-	glfwMakeContextCurrent(window);
-	while (!glfwWindowShouldClose(window))
-	{
-		/* Render here */
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		double xpos, ypos;
-		glfwGetCursorPos(window, &xpos, &ypos);
-
-		//assign all pixels red color 
-		for (int j = 0; j <height; j++)
-			for (int i = 0; i < width; i++) {
-				pixels[(i + width*j) * 3 + 0] = 1.0f;
-				pixels[(i + width*j) * 3 + 1] = 1.0f;
-				pixels[(i + width*j) * 3 + 2] = 1.0f;
-			}
-
-
+		Obj_2D.preprocessing();
 
 		std::vector<GeometricObjectInterface*> obj_list;
 		obj_list.push_back(new GeomericObject<Box>);
@@ -152,23 +73,7 @@ int main() {
 		for (auto itr : obj_list) {
 			(itr)->draw();
 		}
-		
-		
-
-
-		glDrawPixels(width, height, GL_RGB, GL_FLOAT, pixels);
-
-
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
-		std::cout << xpos << "   " << 480 - ypos << std::endl;
-		/* Poll for and process events */
-		glfwPollEvents();
+		Obj_2D.postprocessiong();
 	}
-
-	glfwTerminate();
-
-	delete[] pixels;
-	//free(pixels);
 	return 0;
 }
